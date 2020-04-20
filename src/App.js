9e5053abb445b811c645d1079a3978e7040c4ff7
dianmjs <dianamjs69@gmail.com>
2020-04-20 16:00:19 -0500

    prue212

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numChar: 0,
+    };
+    this.counter = this.counter.bind(this);
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.counter}></textarea>
+        <div className="counter">{this.state.numChar}</div>
       </div>
     );
+  }
+  counter(evet) {
+    this.setState({
+      numChar: this.state.numChar + 1,
+    });
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-import Setup from './Setup';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import Setup from "./Setup";
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   shallow(<App />);
 });
 
-it('renders the textarea', () => {
+it("renders the textarea", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find('textarea').length).toBe(1);
+  expect(wrapper.find("textarea").length).toBe(1);
 });
 
-it('renders the counter', () => {
+it("renders the counter", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find('.counter').length).toBe(1);
-  expect(wrapper.find('.counter').text()).toBe("0");
+  expect(wrapper.find(".counter").length).toBe(1);
+  expect(wrapper.find(".counter").text()).toBe("0");
 });
 
-it('updates the counter', () => {
+it("updates the counter", () => {
   const wrapper = shallow(<App />);
-  wrapper.find('textarea').simulate('change', { target: { value: 'Hola Mundo' } });
-  expect(wrapper.find('.counter').text()).toBe("10");
+  wrapper
+    .find("textarea")
+    .simulate("change", { target: { value: "Hola Mundo" } });
+  expect(wrapper.find(".counter").text()).toBe("1");
 });
